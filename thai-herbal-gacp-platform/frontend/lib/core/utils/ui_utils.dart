548ec1ce8ba8cad10d896d948bcb20007b4b2bf4
jonmaxmore <104246416+jonmaxmore@users.noreply.github.com>
2025-06-28 18:17:01 +0700

    Create ui_utils.dart

---
+++ thai-herbal-gacp-platform/frontend/lib/core/utils/ui_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UIUtils {
+  static void showErrorSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  static void showSuccessSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
